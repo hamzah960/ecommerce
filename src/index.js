06f38739b0ccdbb221b93c4afb10a33e51c37e13
hamza abushamas <hamzah960@gmail.com>
2022-04-29 15:38:00 +0300

    search page responsive finish

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js';
 import '@fortawesome/fontawesome-free/js/all.min';
-
-
+import 'webpack-jquery-ui';
+import 'webpack-jquery-ui/css';
+import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min.js';
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
@@ -125,8 +126,22 @@ $(function () {
         } else {
 
             $('#credit-card-info').show();
-            
+
         }
 
     });
+
+    // مكون البحث حسب السعر
+    $( "#price-range" ).slider({
+        range: true,
+        min: 50,
+        max: 1000,
+        step: 50,
+        values: [ 250, 800 ],
+        slide: function( event, ui ) {
+            $('#price-min').text(ui.values[0]);
+            $('#price-max').text(ui.values[1]);
+        }
+    });
+
 });
